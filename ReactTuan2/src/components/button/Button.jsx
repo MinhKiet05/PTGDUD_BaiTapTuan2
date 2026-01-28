fb06bca26150b5dc2cc8cc81bcdb1b6b3437c8f9
MinhKiet05 <tranvanbetu1965@gmail.com>
2026-01-28 19:56:36 +0700

    cap nhat lai cau truc thu muc

--- ReactTuan2/src/components/button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import "./Button.css"
-export default function Button(props){
-    console.log(props);
-    return <button className={`btn btn-${props.type}`}>
-        {props.children}
-        </button>;
-}
